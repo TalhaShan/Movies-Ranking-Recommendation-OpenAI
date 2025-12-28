2e50440c64f0b105e150f883d4eb3c0a29d51cf2
Talha Shan <talha.shan007@gmail.com>
2025-12-28 15:22:35 +0200

    🚀  Added Open API struct setup for ranking movies.

---
+++ Server/MagicStreamMovieServer/routes/protected_routes.go
@@ -0,0 +1,20 @@
+package routes
+
+
+import (
+	controller "github.com/TalhaShan/Movies-Ranking-Recommendation-OpenAI/Server/MagicStreamMovieServer/controllers"
+	"github.com/TalhaShan/Movies-Ranking-Recommendation-OpenAI/Server/MagicStreamMovieServer/middleware"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+func SetupProtectedRoutes(router *gin.Engine, client *mongo.Client) {
+	router.Use(middleware.AuthMiddleWare())
+
+	router.GET("/movie/:imdb_id", controller.GetMovie(client))
+	router.POST("/addmovie", controller.AddMovie(client))
+	router.GET("/recommendedmovies", controller.GetRecommendedMovies(client))
+	router.PATCH("/updatereview/:imdb_id", controller.AdminReviewUpdate(client))
+}
+
+
